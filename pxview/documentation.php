d46895359625002adc89fba7d84845f56dfe2c8b
steinm <>
2005-02-02 11:35:56 +0000

    - added more manpages

--- pxview/documentation.php
+++ pxview/documentation.php
@@ -17,6 +17,20 @@
 		output_manpage("pxview");
 ?>
 					</p>
+					<p>For those who find the number of options of pxview
+					  to confusing, there is also an extra command for each
+						of the four conversion method. The manual pages document
+						only those options which are available for the requested
+						conversion.
+					</p>
+					<p>
+<?php
+		output_manpage("px2csv");
+		output_manpage("px2html");
+		output_manpage("px2sql");
+		output_manpage("px2sqlite");
+?>
+					</p>
 <?php
 	}
 ?>
