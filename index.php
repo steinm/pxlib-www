f030ae3de65d81271eb7dbcf5a7964fddcea7e7a
steinm <>
2006-02-13 16:56:54 +0000

    - added section about software that uses pxlib

--- index.php
+++ index.php
@@ -19,6 +19,28 @@
 							<li>Reading/writing encrypted files, even without knowing the password</li>
 						</ul>
 					</p>
+					<h2>Software that uses pxlib</h2>
+					<p>
+						There are currently two larger projects using pxlib.
+						<ul>
+							<li>gnumeric</li>
+							<li>knoda (hk_classes)</li>
+						</ul>
+					</p>
+					<p>
+						The <a href="www.gnumeric.org">gnumeric</a> plugin is written by
+						the author of pxlib itself and
+						is currently able to read paradox databases which do not contain
+						any blobs. Write support will be available in gnumeric 1.7.0 if
+						the <a href="http://bugzilla.gnome.org/show_bug.cgi?id=329771">patch in the bug database</a> will make it into the code in time.
+					</p>
+					<p>
+						<a href="http://www.knoda.org">knoda</a> uses pxlib through its
+						database abstraction classes
+						<a href="http://hk­classes.sourceforge.net">hk_classes</a>.
+						Recent versions have also added support for
+						writing and modifying paradox databases.
+					</p>
 <?php
 	include("footer.inc");
 ?>
